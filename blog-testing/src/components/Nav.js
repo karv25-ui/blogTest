3a30ef907c7cf60edbc355986dc5c394cf824efb
karv25-ui <karvenda@gmail.com>
2026-01-05 19:34:40 +0000

    added more code

---
+++ blog-testing/src/components/Nav.js
@@ -0,0 +1,16 @@
+function Nav() {
+    return (
+        <nav className="main-nav">
+            <ul>
+                <li>
+                <a href="./Nav.js">Home</a>
+                </li>
+                <li>Articles</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
--- blog-testing/src/index.js
+++ blog-testing/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Nav from './components/Nav';
+import Promo from './components/Promo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Nav />
+    <Promo />
   </React.StrictMode>
 );
 
