766e4324981e4f93cb1a6cab2fe3a0ebd47ea3f0
karv25-ui <karvenda@gmail.com>
2026-07-22 16:00:45 +0000

    Learned how to render HTML frfr

--- blog-testing/src/index.js
+++ blog-testing/src/index.js
@@ -7,15 +7,31 @@ import Nav from './components/Nav';
 import Promo from './components/Promo';
 import Footer from './components/Footer';
 
+const myElement = (
+<table>
+  <tr>
+    <th>First Name</th>
+    <th>Last Name</th>
+  </tr>
+  <tr>
+    <td>Jane</td>
+    <td>Doe</td>
+  </tr>
+</table>
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {myElement}
     <Nav />
     <Promo />
     <Footer />
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
